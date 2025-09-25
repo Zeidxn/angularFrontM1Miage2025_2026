01042a4aba857ca257a3288eabae6bdd4bd4695c
Nicolas Toupin <88687413+NKT04@users.noreply.github.com>
2025-09-25 09:09:31 +0200

    button exercices

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDivider } from '@angular/material/divider';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatButtonModule, MatDivider, MatIcon],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
